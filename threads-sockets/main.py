b06f463ef39167bdfeba33962ae65af1fda889c4
Hozek-V <janvystup@seznam.cz>
2024-08-26 21:23:34 +0200

    update

--- threads-sockets/main.py
+++ threads-sockets/main.py
@@ -8,8 +8,16 @@ def print_numbers():
         print(f"Vlákno {threading.current_thread().name}: {i}")
 
 
+def sum_numbers():
+    result = 0
+    for i in range(5):
+        time.sleep(1)
+        result += i
+        print(f"Vlákno {threading.current_thread().name}: {result}")
+
+
 thread1 = threading.Thread(target=print_numbers, name="Vlákno 1")
-thread2 = threading.Thread(target=print_numbers, name="Vlákno 2")
+thread2 = threading.Thread(target=sum_numbers, name="Vlákno 2")
 
 thread1.start()
 thread2.start()
